6207fa8d01ea40cfd49ed008f68a3a4ed2b3df32
DmitriyNozdrin <multik83@bk.ru>
2017-02-24 22:44:57 +0200

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.text.DateFormat;
 import java.text.ParseException;
-import  java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
@@ -13,46 +13,47 @@ public class Main {
         // write your code here
 
 
+        Student s1 = new Student("Nz", "Dm", "Ur", "16.08.1983", "Kharkov", 8067, "IT", 5, "Kit-17");
+        Student s2 = new Student("s2", "Mm", "TT", "01.09.1989", "kiev", 066167, "PT", 4, "KZ");
+        Student s3 = new Student("s3k", "s3", "Uiir", "16.07.2000", "Kharkov", 8067, "IT", 1, "Kit-16");
+        Student s4 = new Student("34", "s4", "ioT", "01.09.1995", "lviv", 066167, "hz", 2, "KZ");
 
-        Student s1 = new Student ("Nz", "Dm", "Ur", "16.08.1983", "Kharkov", 8067, "IT", 5, "Kit-17" );
-        Student s2 = new Student ("s2", "Mm", "TT", "01.09.1989", "kiev", 066167, "PT", 4, "KZ" );
-        Student s3 = new Student ("s3k", "s3", "Uiir", "16.07.2000", "Kharkov", 8067, "IT", 1, "Kit-16" );
-        Student s4 = new Student ("34", "s4", "ioT", "01.09.1995", "lviv", 066167, "hz", 2, "KZ" );
 
+        Student arr[] = {s1, s2, s3, s4};
 
-        Student arr [] = {s1, s2,s3,s4};
+        for (int i = 0; i < arr.length; i++) {
+            String fc[];
 
-        for (int i = 0; i <arr.length ; i++) {
-            String fuc [];
-           
             if (arr[i].getFac().equals(arr[i + 1]))
                 continue;
             else {
-                fuc[i] = arr[i].getFac();
+                fc[i] = arr[i].getFac();
 
             }
-            System.out.println(arr[i].getFac());
+            System.out.println(arr[i].getStudent());
         }
 
 
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter Facultet");
-    String kursIn = scan.next();
-            for (int i = 0; i <arr.length ; i++) {
-            if (arr[i].getFac().equals(kursIn))
-                arr[i].getStudent();
+        String kursIn = scan.next();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getFac().equals(kursIn)) ;
+            else continue;
+
+            arr[i].getStudent();
 
         }
         System.out.println("Enter date of birth");
-            String d = scan.next();
-            DateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+        String d = scan.next();
+        DateFormat format = new SimpleDateFormat("dd.mm.yyyy");
         Date inputDate = null;
         try {
             inputDate = format.parse(d);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i <arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             Date studentDate = null;
             try {
                 studentDate = format.parse(arr[i].getD_date());
@@ -60,7 +61,7 @@ public class Main {
                 e.printStackTrace();
             }
 
-            if (inputDate.before(studentDate)){
+            if (inputDate.before(studentDate)) {
                 arr[i].getStudent();
             }
 
@@ -75,6 +76,6 @@ public class Main {
 
             }*/
 
-        }
     }
+}
 
